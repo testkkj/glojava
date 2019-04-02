ebea26963fa9dabbf2a8e7918eb6f524b3ec11ac
testkkj <kimkj1212@naver.com>
2019-04-02 15:50:50 +0900

    chapter01 upload

---
+++ Chap01/src/example/RunStatementExample.java
@@ -0,0 +1,15 @@
+package example;
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+		
+		int x = 1;	//변수 x를 선언하고 1을 저장
+		int y = 1;	//변수 y를 선언하고 1을 저장
+		int result = x + y;	//변수 result를 선언하고 x와  y를 더한 값을 저장
+		
+		System.out.println(x + " + " + y + " = " + result); //콘솔에 출력하는 메소드 호출
+		
+	}
+
+}
